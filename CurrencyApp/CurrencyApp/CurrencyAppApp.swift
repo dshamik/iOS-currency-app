9be77a573e14f86d8e609147890f85c588828843
dshamik <dinar4ik2002@gmail.com>
2023-11-15 16:28:51 +0300

    start project

---
+++ CurrencyApp/CurrencyApp/CurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyAppApp.swift
+//  CurrencyApp
+//
+//  Created by Динар Шамсутдинов on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
